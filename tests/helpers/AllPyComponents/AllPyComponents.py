1d410a1b91e4632820dc253fad8f3e3f381606b6
Geet Kalra <geet.kalra@intersystems.com>
2025-11-17 00:19:19 -0500

    add some test cases for automated tests and cpf file required to start iris container for automated tests when github workflows are setup

---
+++ tests/helpers/AllPyComponents/AllPyComponents.py
@@ -0,0 +1,72 @@
+from intersystems_pyprod import (
+    IRISParameter,
+    IRISProperty,
+    BusinessService,
+    BusinessProcess,
+    BusinessOperation,
+    OutboundAdapter,
+    Column,
+    JsonSerialize,
+    PickleSerialize,
+    IRISLog,
+    Status,
+    debug_host,
+)
+
+package_name = "AllPyComponents"
+
+class MyJsonData(JsonSerialize):
+    name = Column(default="default name")
+    amount = Column(default=0)
+
+class MyPickleData(PickleSerialize):
+    name = Column()
+    amount = 1
+
+class AdapterlessBS(BusinessService):
+    TargetConfigName = IRISProperty(settings="TargetConfigName:Target")
+    def OnProcessInput(self, input):
+        status = Status.OK()
+        msg = MyJsonData(input)
+        status, response = self.SendRequestSync(self.TargetConfigName, msg)
+        return status, response
+    
+class CustomBP(BusinessProcess):
+    TargetConfigName: str = IRISProperty(settings="TargetConfigName:Target")
+    def OnRequest(self, request):
+        status = Status.OK()
+        if request.name == "testMyJson" :
+            syncRequest = MyJsonData("MyJsonData request from BP to BO", 1)
+        elif request.name == "testMyPickle":
+            syncRequest = MyPickleData("MyPickleData request from BP to BO", 1)
+        status, response = self.SendRequestSync(self.TargetConfigName, syncRequest)
+        return status, response
+
+
+class CustomBO(BusinessOperation):
+    ADAPTER = IRISParameter("AllPyComponents.CustomOutAdapter")
+    MessageMap = {
+        "AllPyComponents.MyJsonData": "BOmethod1",
+        "AllPyComponents.MyPickleData": "BOmethod2"
+    }
+
+    def BOmethod1(self, request):
+        status = Status.OK()
+        IRISLog.Info("Data received at BOmethod1 is: " + request.name)
+        self.ADAPTER.OutAdapterMethod("From BOmethod1")
+        response = MyJsonData("response from BOmethod1", 0)
+        return status, response
+
+    def BOmethod2(self, request):
+        status = Status.OK()
+        IRISLog.Info("Data received at BOmethod2 is: " + request.name)
+        response = MyPickleData("response from BOmethod2", 0)
+        return status, response
+  
+
+class CustomOutAdapter(OutboundAdapter):
+    def OutAdapterMethod(self, information="default"):
+        status = Status.OK()
+        IRISLog.Info("Data received at Outbound Adapter is: " + information)
+        return status
+
---
+++ tests/helpers/OsPyMixed/TCPAdaptersFromOs/TCPAdaptersFromOs.py
@@ -0,0 +1,28 @@
+from intersystems_pyprod import ( IRISParameter, IRISProperty,Status,
+    BusinessService, BusinessOperation, JsonSerialize,IRISLog)
+
+
+package_name = "TCPAdaptersFromOs"
+
+class simplemsg(JsonSerialize):
+    value: str
+
+class TCPBusinessService(BusinessService):
+    ADAPTER = IRISParameter("EnsLib.TCP.TextLineInboundAdapter")
+    TargetConfig = IRISProperty(settings="TargetConfig:SelectTarget")
+    def OnProcessInput(self, input):
+        status = Status.OK()
+        mymsg = simplemsg()
+        mymsg.value = input.StringValue
+        IRISLog.Info("message received at business service"+ mymsg.value)
+        status = self.SendRequestAsync("TCPAdaptersFromOs.TCPBusinessOperation", mymsg)
+        return status
+
+class TCPBusinessOperation(BusinessOperation):
+    ADAPTER = IRISParameter("EnsLib.TCP.TextLineOutboundAdapter")
+    MessageMap = {"TCPAdaptersFromOs.simplemsg": "sendout"}
+    def sendout(self,input):
+        status = Status.OK()
+        IRISLog.Info("recevied at operation: "+str(input.value))
+        self.iris_host_object.Adapter.SendMessageString(input.value)
+        return status
